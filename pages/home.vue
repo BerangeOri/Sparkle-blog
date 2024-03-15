c94bd8dfea77dd8b8066cbc7230de1209a094d41
BerangeOri <suk97606@gmail.com>
2024-03-16 02:48:50 +0900

    names brief

--- pages/home.vue
+++ pages/home.vue
@@ -2,17 +2,21 @@
   <v-card dark class="pa-3">
     <v-responsive :aspect-ratio="16 / 9">
       <!-- <p>Home</p> -->
-      {{ gamePostnow }}
+      <!-- {{ gamePostnow }} -->
       <!-- {{ Object.keys(blogPostnow) ?? '' }} -->
+      {{ gamePostnow }}
       <v-expansion-panels>
-        <v-expansion-panel v-for="(name, i) in Object.keys(categories)" bg-color="secondary "
+        <v-expansion-panel
+          v-for="(name, i) in Object.keys(categories)"
+          bg-color="secondary "
           focusable
         >
           <v-expansion-panel-title expand-icon="mdi-menu-down">
             {{ name }}
           </v-expansion-panel-title>
           <v-expansion-panel-text>
-            <v-data-iterator v-for="(postype, j) in Object.keys(categories[name])"
+            <v-data-iterator
+              v-for="(postype, j) in Object.keys(categories[name])"
               :items="categories[name][postype]"
               items-per-page="-1"
             >
@@ -20,16 +24,20 @@
                 {{ postype }}
               </template>
               <template v-slot:default="{ items }">
-                <v-card v-for="item in items">
-                  <v-card-title>{{ item["title"] }}</v-card-title>
-                </v-card>
+                <v-col>
+                  <v-container v-for="item in items">
+                    <v-card width="20vw">
+                      <v-card-title>{{ item.raw.title }}</v-card-title>
+                      <v-card-text> {{ item.raw.breif }} </v-card-text>
+                    </v-card>
+                  </v-container>
+                </v-col>
               </template>
             </v-data-iterator>
           </v-expansion-panel-text>
         </v-expansion-panel>
       </v-expansion-panels>
-      <v-btn color="success" @click="BlogStore.loadRecent()"
-      >text</v-btn>
+      <v-btn color="success" @click="BlogStore.loadRecent()">text</v-btn>
     </v-responsive>
   </v-card>
 </template>
@@ -42,18 +50,17 @@ import { NotionRenderer, getPageBlocks, getPageTable } from 'vue3-notion'
 
 import { ref, onMounted } from 'vue'
 
-import { useGameStore } from '~/store/blog';
-import { storeToRefs } from 'pinia';
+import { useGameStore } from '~/store/blog'
+import { storeToRefs } from 'pinia'
 const BlogStore = useGameStore()
 const { gamePostnow } = storeToRefs(BlogStore)
 
-const categories = ref<Record<string,any>>({})
+const categories = ref<Record<string, any>>({})
 
 onMounted(async () => {
   BlogStore.loadRecent()
-  categories.value["GAMES"] = gamePostnow
+  categories.value['GAMES'] = gamePostnow
 })
-
 </script>
 
 <style scoped></style>
